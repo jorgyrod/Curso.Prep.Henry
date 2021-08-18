afe01aebe8fcdb1f7fc9e3630665e58e3706652e
Jorgy Rodriguez <jorgsrodriguez@gmail.com>
2021-08-17 23:39:46 -0500

    Solucion de ejercicios JS 07, funciones de arreglos y adicion de un archivo js de prueba para utilizar la funcion filter de array

---
+++ 07-JS-VI/homework/Ultimo Ejercicio.js
@@ -0,0 +1,10 @@
+var nuevoArray = ["monica", "andrea", "camila", "Juana", "ana"];
+
+var palabras = [];
+
+
+nuevoArray.filter(elemento => elemento.charAt(0) === "a");
+
+
+palabras = nuevoArray.filter(elemento => elemento.charAt(0) === "m");
+
